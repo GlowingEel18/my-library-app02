8ed8b5800f1c64e5b4803724f9a954757cec947f
Hardik A Marlapudi <hardik.a.marlapudi@gmail.com>
2024-11-16 16:56:28 -0500

    fixed backend issues

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const app = express();
 const Joi = require("joi");
@@ -7,7 +6,6 @@ const multer = require("multer");
 const path = require("path");
 
 app.use(cors());
-app.use(fileUpload);
 app.use(express.json()); // Parse JSON bodies for POST requests
 app.use(express.static("public"));
 
@@ -25,265 +23,294 @@ const upload = multer({ storage: storage });
 
 // Book data
 const books = [
-    {
-    "_id": "1",
-    "title": "1984",
-    "description": "A dystopian novel by George Orwell",
-    "image": "images/1984.jpg"
-    },
-    {
-    "_id": "2",
-    "title": "Andromeda",
-    "description": "A novel set in a sci-fi universe",
-    "image": "images/Andromeda.jpg"
-    },
-    {
-    "_id": "3",
-    "title": "Angels & Demons",
-    "description": "A mystery thriller novel by Dan Brown",
-    "image": "images/Angels&Demons.jpg"
-    },
-    {
-    "_id": "4",
-    "title": "Becoming",
-    "description": "A memoir by Michelle Obama",
-    "image": "images/Becoming.jpg"
-    },
-    {
-    "_id": "5",
-    "title": "Big Little Lies",
-    "description": "A novel by Liane Moriarty",
-    "image": "images/BigLittleLies.jpg"
-    },
-    {
-    "_id": "6",
-    "title": "Desire",
-    "description": "A historical novel by Pulitzer Prize winner",
-    "image": "images/Desire.jpg"
-    },
-    {
-    "_id": "7",
-    "title": "Dracula",
-    "description": "A psychological novel by Bram Stoker",
-    "image": "images/dracula.jpg"
-    },
-    {
-    "_id": "8",
-    "title": "Dune",
-    "description": "A novel by Frank Herbert",
-    "image": "images/Dune.jpg"
-    },
-    {
-    "_id": "9",
-    "title": "Educated",
-    "description": "A fantasy novel by Tara Westover",
-    "image": "images/Educated.jpg"
-    },
-    {
-    "_id": "10",
-    "title": "Enchanted Forest",
-    "description": "A magical story set in an enchanted forest",
-    "image": "images/EnchantedForest.jpg"
-    },
-    {
-    "_id": "11",
-    "title": "Fahrenheit 451",
-    "description": "A dystopian novel by Ray Bradbury",
-    "image": "images/Fahrenheit.jpg"
-    },
-    {
-    "_id": "12",
-    "title": "Fantasy",
-    "description": "A collection of fantasy stories",
-    "image": "images/Fantasy.jpg"
-    },
-    {
-    "_id": "13",
-    "title": "Fiction",
-    "description": "Various fictional stories",
-    "image": "images/Fiction.jpg"
-    },
-    {
-    "_id": "14",
-    "title": "Foundation",
-    "description": "A science fiction novel by Isaac Asimov",
-    "image": "images/Foundation.jpg"
-    },
-    {
-    "_id": "15",
-    "title": "Frankenstein",
-    "description": "A Gothic novel by Mary Shelley",
-    "image": "images/frankenstein.jpg"
-    },
-    {
-    "_id": "16",
-    "title": "Gory Details",
-    "description": "An exploration of the dark side of nature",
-    "image": "images/GoryDetails.jpg"
-    },
-    {
-    "_id": "17",
-    "title": "Hamlet",
-    "description": "A tragedy by William Shakespeare",
-    "image": "images/Hamlet.jpg"
-    },
-    {
-    "_id": "18",
-    "title": "Harry Potter and the Philosopher's Stone",
-    "description": "A fantasy novel by J.K. Rowling",
-    "image": "images/HarryPotterThrone.jpg"
-    },
-    {
-    "_id": "19",
-    "title": "Horror",
-    "description": "A collection of horror stories",
-    "image": "images/Horror.jpg"
-    },
-    {
-    "_id": "20",
-    "title": "Hound",
-    "description": "A detective novel by Arthur Conan Doyle",
-    "image": "images/Hound.jpg"
-    },
-    {
-    "_id": "21",
-    "title": "Humankind",
-    "description": "A non-fiction book about humanity",
-    "image": "images/Humankind.jpg"
-    },
-    {
-    "_id": "22",
-    "title": "Ice & Fire",
-    "description": "A fantasy novel in a medieval world",
-    "image": "images/Ice&Fire.jpg"
-    },
-    {
-    "_id": "23",
-    "title": "Me Before You",
-    "description": "A romance novel by Jojo Moyes",
-    "image": "images/me_before_you.jpg"
-    },
-    {
-    "_id": "24",
-    "title": "Mystery",
-    "description": "A collection of mystery stories",
-    "image": "images/Mystery.jpg"
-    },
-    {
-    "_id": "25",
-    "title": "Neuromancer",
-    "description": "A cyberpunk novel by William Gibson",
-    "image": "images/Neuromancer.jpg"
-    },
-    {
-    "_id": "26",
-    "title": "New Novels",
-    "description": "A collection of new novels",
-    "image": "images/New Novels.jpg"
-    },
-    {
-    "_id": "27",
-    "title": "Non-Fiction",
-    "description": "Various non-fiction works",
-    "image": "images/NonFiction.jpg"
-    },
-    {
-    "_id": "28",
-    "title": "Periodic Tables",
-    "description": "A book on chemistry",
-    "image": "images/Periodic Tables.jpg"
-    },
-    {
-    "_id": "29",
-    "title": "Pride and Prejudice",
-    "description": "A romance novel by Jane Austen",
-    "image": "images/Prejudice.jpg"
-    },
-    {
-    "_id": "30",
-    "title": "Romance",
-    "description": "A collection of romance stories",
-    "image": "images/Romance.jpg"
-    },
-    {
-    "_id": "31",
-    "title": "Salesman",
-    "description": "A story about the life of a salesman",
-    "image": "images/Salesman.jpg"
-    },
-    {
-    "_id": "32",
-    "title": "Sci-Fi",
-    "description": "A collection of sci-fi stories",
-    "image": "images/Sci-Fi.jpg"
-    },
-    {
-    "_id": "33",
-    "title": "Scientific Mystery",
-    "description": "Mysteries based on science",
-    "image": "images/Scientific Mystery.jpg"
-    },
-    {
-    "_id": "34",
-    "title": "Shadow",
-    "description": "A novel exploring the darkness of society",
-    "image": "images/Shadow.jpg"
-    },
-    {
-    "_id": "35",
-    "title": "Silent Observer",
-    "description": "A book on the power of silence",
-    "image": "images/silentObserver.jpg"
-    },
-    {
-    "_id": "36",
-    "title": "Strange Chemistry",
-    "description": "A book on unusual chemical reactions",
-    "image": "images/StrangeChemistry.jpg"
-    },
-    {
-    "_id": "37",
-    "title": "The Awakening",
-    "description": "A novel by Kate Chopin about self-discovery",
-    "image": "images/TheAwakening.jpg"
-    },
-    {
-    "_id": "38",
-    "title": "The Shining",
-    "description": "A horror novel by Stephen King",
-    "image": "images/TheShining.jpg"
-    },
-    {
-    "_id": "39",
-    "title": "The Silent Observer",
-    "description": "An observer’s perspective on life",
-    "image": "images/TheSilentObserver.jpg"
-    },
-    {
-    "_id": "40",
-    "title": "To Kill a Mockingbird",
-    "description": "A novel by Harper Lee",
-    "image": "images/To Kill a MockingBird.jpg"
-    },
-    {
-    "_id": "41",
-    "title": "Whispers Wind",
-    "description": "A collection of whispers from nature",
-    "image": "images/WhispersWind.jpg"
-    }
-];  
+  {
+    _id: 1,
+    title: "1984",
+    description: "A dystopian novel by George Orwell",
+    main_image: "images/1984.jpg",
+  },
+  {
+    _id: 2,
+    title: "The Great Gatsby",
+    description: "A classic novel by F. Scott Fitzgerald",
+    main_image: "images/gatsby.jpg",
+  },
+  {
+    _id: 3,
+    title: "To Kill a Mockingbird",
+    description: "A novel by Harper Lee about racial injustice",
+    main_image: "images/mockingbird.jpg",
+  },
+  {
+    _id: 4,
+    title: "Moby Dick",
+    description: "A novel about a quest to hunt a giant whale",
+    main_image: "images/mobydick.jpg",
+  },
+  {
+    _id: 5,
+    title: "War and Peace",
+    description: "A historical novel by Leo Tolstoy",
+    main_image: "images/warandpeace.jpg",
+  },
+  {
+    _id: 6,
+    title: "Pride and Prejudice",
+    description: "A romantic novel by Jane Austen",
+    main_image: "images/prideandprejudice.jpg",
+  },
+  {
+    _id: 7,
+    title: "Crime and Punishment",
+    description: "A psychological novel by Fyodor Dostoevsky",
+    main_image: "images/crimeandpunishment.jpg",
+  },
+  {
+    _id: 8,
+    title: "Brave New World",
+    description: "A dystopian novel by Aldous Huxley",
+    main_image: "images/bravenewworld.jpg",
+  },
+  {
+    _id: 9,
+    title: "The Catcher in the Rye",
+    description: "A novel by J.D. Salinger",
+    main_image: "images/catcherintherye.jpg",
+  },
+  {
+    _id: 10,
+    title: "The Hobbit",
+    description: "A fantasy novel by J.R.R. Tolkien",
+    main_image: "images/hobbit.jpg",
+  },
+  {
+    _id: 11,
+    title: "Jane Eyre",
+    description: "A novel by Charlotte Bronte",
+    main_image: "images/janeeyre.jpg",
+  },
+  {
+    _id: 12,
+    title: "Wuthering Heights",
+    description: "A novel by Emily Bronte",
+    main_image: "images/wutheringheights.jpg",
+  },
+  {
+    _id: 13,
+    title: "The Odyssey",
+    description: "An epic poem attributed to Homer",
+    main_image: "images/odyssey.jpg",
+  },
+  {
+    _id: 14,
+    title: "The Iliad",
+    description: "An ancient Greek epic poem attributed to Homer",
+    main_image: "images/iliad.jpg",
+  },
+  {
+    _id: 15,
+    title: "Hamlet",
+    description: "A tragedy by William Shakespeare",
+    main_image: "images/hamlet.jpg",
+  },
+  {
+    _id: 16,
+    title: "The Divine Comedy",
+    description: "An epic poem by Dante Alighieri",
+    main_image: "images/divinecomedy.jpg",
+  },
+  {
+    _id: 17,
+    title: "The Brothers Karamazov",
+    description: "A novel by Fyodor Dostoevsky",
+    main_image: "images/karamazov.jpg",
+  },
+  {
+    _id: 18,
+    title: "The Count of Monte Cristo",
+    description: "A novel by Alexandre Dumas",
+    main_image: "images/montecristo.jpg",
+  },
+  {
+    _id: 19,
+    title: "The Picture of Dorian Gray",
+    description: "A novel by Oscar Wilde",
+    main_image: "images/doriangray.jpg",
+  },
+  {
+    _id: 20,
+    title: "The Scarlet Letter",
+    description: "A novel by Nathaniel Hawthorne",
+    main_image: "images/scarletletter.jpg",
+  },
+  {
+    _id: 21,
+    title: "Anna Karenina",
+    description: "A novel by Leo Tolstoy",
+    main_image: "images/annakarenina.jpg",
+  },
+  {
+    _id: 22,
+    title: "Don Quixote",
+    description: "A novel by Miguel de Cervantes",
+    main_image: "images/donquixote.jpg",
+  },
+  {
+    _id: 23,
+    title: "One Hundred Years of Solitude",
+    description: "A novel by Gabriel Garcia Marquez",
+    main_image: "images/solitude.jpg",
+  },
+  {
+    _id: 24,
+    title: "The Alchemist",
+    description: "A novel by Paulo Coelho",
+    main_image: "images/alchemist.jpg",
+  },
+  {
+    _id: 25,
+    title: "The Old Man and the Sea",
+    description: "A novel by Ernest Hemingway",
+    main_image: "images/oldmansea.jpg",
+  },
+  {
+    _id: 26,
+    title: "The Lord of the Rings",
+    description: "An epic fantasy novel by J.R.R. Tolkien",
+    main_image: "images/lotr.jpg",
+  },
+  {
+    _id: 27,
+    title: "Les Miserables",
+    description: "A novel by Victor Hugo",
+    main_image: "images/lesmiserables.jpg",
+  },
+  {
+    _id: 28,
+    title: "A Tale of Two Cities",
+    description: "A novel by Charles Dickens",
+    main_image: "images/taleoftwocities.jpg",
+  },
+  {
+    _id: 29,
+    title: "Alice's Adventures in Wonderland",
+    description: "A novel by Lewis Carroll",
+    main_image: "images/alice.jpg",
+  },
+  {
+    _id: 30,
+    title: "Heart of Darkness",
+    description: "A novel by Joseph Conrad",
+    main_image: "images/heartofdarkness.jpg",
+  },
+  {
+    _id: 31,
+    title: "Dracula",
+    description: "A horror novel by Bram Stoker",
+    main_image: "images/dracula.jpg",
+  },
+  {
+    _id: 32,
+    title: "Frankenstein",
+    description: "A novel by Mary Shelley",
+    main_image: "images/frankenstein.jpg",
+  },
+  {
+    _id: 33,
+    title: "The Call of the Wild",
+    description: "A novel by Jack London",
+    main_image: "images/callofthewild.jpg",
+  },
+  {
+    _id: 34,
+    title: "The Sun Also Rises",
+    description: "A novel by Ernest Hemingway",
+    main_image: "images/sunalsorises.jpg",
+  },
+  {
+    _id: 35,
+    title: "The Metamorphosis",
+    description: "A novella by Franz Kafka",
+    main_image: "images/metamorphosis.jpg",
+  },
+  {
+    _id: 36,
+    title: "Ulysses",
+    description: "A novel by James Joyce",
+    main_image: "images/ulysses.jpg",
+  },
+  {
+    _id: 37,
+    title: "Slaughterhouse-Five",
+    description: "A novel by Kurt Vonnegut",
+    main_image: "images/slaughterhousefive.jpg",
+  },
+  {
+    _id: 38,
+    title: "Catch-22",
+    description: "A novel by Joseph Heller",
+    main_image: "images/catch22.jpg",
+  },
+  {
+    _id: 39,
+    title: "The Catcher in the Rye",
+    description: "A novel by J.D. Salinger",
+    main_image: "images/catcherintherye.jpg",
+  },
+  {
+    _id: 40,
+    title: "Fahrenheit 451",
+    description: "A dystopian novel by Ray Bradbury",
+    main_image: "images/fahrenheit451.jpg",
+  },
+  {
+    _id: 41,
+    title: "Maus",
+    description: "A graphic novel by Art Spiegelman",
+    main_image: "images/maus.jpg",
+  },
+  {
+    _id: 42,
+    title: "Beloved",
+    description: "A novel by Toni Morrison",
+    main_image: "images/beloved.jpg",
+  },
+  {
+    _id: 43,
+    title: "The Road",
+    description: "A novel by Cormac McCarthy",
+    main_image: "images/theroad.jpg",
+  },
+  {
+    _id: 44,
+    title: "The Grapes of Wrath",
+    description: "A novel by John Steinbeck",
+    main_image: "images/grapesofwrath.jpg",
+  },
+  {
+    _id: 45,
+    title: "The Stranger",
+    description: "A novel by Albert Camus",
+    main_image: "images/thestranger.jpg",
+  },
+]; 
 
 // Serve the main HTML file
 app.get("/", (req, res) => {
+  console.log('returning  hello world');
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Endpoint to get all books
 app.get("/api/books", (req, res) => {
-  console.log('Received request for books');
+  console.log('returning  hello world');
   res.json(books);
-  console.log('response books count: %d', books.length); 
+  console.log('returning ', books.length);
 });
+
+// Endpoint to add a new book
+// app.post("/api/books", upload.single("image"), (req, res) => {
+//   console.log("Received a POST request to add a book");
 
 // Endpoint to add a new book
 // app.post("/api/books", upload.single("image"), (req, res) => {
@@ -319,6 +346,8 @@ app.get("/api/books", (req, res) => {
 //FIXME 
 // Upload Endpoint That will accept files
 app.post("/api/books", (req, res) => {
+
+  console.log('Inside Post method');
   console.log(req.files);
  
   // Check if file is not available return message with status 400.
